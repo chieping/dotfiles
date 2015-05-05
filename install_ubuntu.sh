4c0935a8132081acdec10bbd68704b2f962d17ae
chieping <takashi.honda.01@gmail.com>
2015-05-05 19:18:39 +0900

    update

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -11,60 +11,14 @@ fi
 
 # This script is WIP
 
-# Symlinks
-ln -sf ~/.dotfiles/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/ctags ~/.ctags
-ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/gitignore ~/.gitignore
-ln -sf ~/.dotfiles/gvimrc ~/.gvimrc
-ln -sf ~/.dotfiles/irbrc ~/.irbrc
-ln -sf ~/.dotfiles/pryrc ~/.pryrc
-ln -sf ~/.dotfiles/rubocop.yml ~/.rubocop.yml
-ln -sf ~/.dotfiles/tigrc ~/.tigrc
-ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/vimperatorrc ~/.vimperatorrc
-ln -sf ~/.dotfiles/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zshrc.darwin ~/.zshrc.darwin
-ln -sf ~/.dotfiles/zshrc.linux ~/.zshrc.linux
-ln -sf ~/.dotfiles/gemrc ~/.gemrc
-
-mkdir -p ~/.atom
-ln -sf ~/.dotfiles/atom/config.cson ~/.atom/config.cson
-ln -sf ~/.dotfiles/atom/init.coffee ~/.atom/init.coffee
-ln -sf ~/.dotfiles/atom/keymap.cson ~/.atom/keymap.cson
-ln -sf ~/.dotfiles/atom/snippets.cson ~/.atom/snippets.cson
-ln -sf ~/.dotfiles/atom/styles.less ~/.atom/styles.less
-
-mkdir -p ~/.rbenv
-ln -sf ~/.dotfiles/rbenv/default-gems ~/.rbenv/default-gems
-
-mkdir -p ~/.vim
-ln -sf ~/.dotfiles/vim/filetype.vim ~/.vim/filetype.vim
-
-mkdir -p ~/.git_templates/hooks
-ln -sf ~/.dotfiles/git_templates/hooks/pre-commit ~/.git_templates/hooks/pre-commit
-
-mkdir -p ~/.vim/{backups,swaps,undo}
-
-mkdir -p ~/bin
-
-
 # Vim
-#   neobundle install
 ##   ctags install
 
 # go
 # tig
 # tmux
-# tmux package manager
-# tmux color scheme ( tmux-colors-solarized )
 # ag
 # xclip or xsel (tmux-yank)
-
-
-# oh-my-zsh
-# oh-my-zsh custom plugins ( zsh-syntax-highlighting k )
 
 
 # rainbarf
@@ -91,13 +45,6 @@ go get -u github.com/laurent22/massren
 go get -u github.com/peco/peco/cmd/peco
 # }}}
 
-# rbenv {{{
-ghq get sstephenson/rbenv
-ln -s ~/src/github.com/sstephenson/rbenv ~/.rbenv
-# }}}
-
-# rbenv plugins ( ruby-build default-gems )
-
 # Fonts ( Ricty )
 
 # IME (Mozc)
@@ -107,12 +54,6 @@ ln -s ~/src/github.com/sstephenson/rbenv ~/.rbenv
 # zeal( dash for Linux and Windows )
 
 # firefox plugins ( vimperator )
-
-# vimperator plugins {{{
-mkdir -p ~/.vimperator/plugin
-ghq get vimpr/vimperator-plugins
-ln -sf ~/src/github.com/vimpr/vimperator-plugins/plugin_loader.js ~/.vimperator/plugin/plugin_loader.js
-# }}}
 
 # aws cli
 # aws_completer
