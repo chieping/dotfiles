f052bb55a7461adb618da8da0b46961981ce8600
chieping <takashi.honda.01@gmail.com>
2017-06-19 14:52:36 +0900

    update

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,6 +1,8 @@
 // https://github.com/brookhong/Surfingkeys
 // url: https://raw.githubusercontent.com/chieping/dotfiles/master/surfingkeys.js
 
+/* eslint-disable no-undef, strict */
+
 // an example to create a new mapping `ctrl-y`
 mapkey('<Ctrl-y>', 'Show me the money', function() {
     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
@@ -12,28 +14,25 @@ map('?', 'u');
 // an example to remove mapkey `Ctrl-i`
 unmap('<Ctrl-i>');
 
-// click `Save` button to make above settings to take effect.
-// set theme
-settings.theme = `
-.sk_theme {
-    background: #000;
-    color: #fff;
-}
-.sk_theme tbody {
-    color: #000;
-}
-.sk_theme input {
-    color: #317ef3;
-}
-.sk_theme .url {
-    color: #38f;
-}
-.sk_theme .annotation {
-    color: #38f;
-}
-
-.sk_theme .focused {
-    background: #aaa;
-}`;
+settings.theme = '\
+.sk_theme { \
+    background: #fff; \
+    color: #000; \
+} \
+.sk_theme tbody { \
+    color: #000; \
+} \
+.sk_theme input { \
+    color: #000; \
+} \
+.sk_theme .url { \
+    color: #555; \
+} \
+.sk_theme .annotation { \
+    color: #555; \
+} \
+.sk_theme .focused { \
+    background: #f0f0f0; \
+}';
 
 settings.smoothScroll = false;
