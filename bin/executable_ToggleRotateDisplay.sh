a1961f33be2f586293eb2166b134c62430ef7127
chieping <takashi.honda.01@gmail.com>
2023-09-07 23:37:03 +0900

    update

--- bin/executable_ToggleRotateDisplay.sh
+++ bin/executable_ToggleRotateDisplay.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-info=($(RotateDisplay | grep 'DELL U2720Q' | awk '{print $1, $4}' | tr -d °))
+info=($(RotateDisplay | grep 'EV3285' | awk '{print $1, $4}' | tr -d °))
 desired_rotation=$(expr 90 - ${info[1]})
 env RotateDisplay ${info[0]} $desired_rotation
