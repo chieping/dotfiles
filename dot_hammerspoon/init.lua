4b2b54ec98d0339018907a3c098d781a55d823c6
chieping <takashi.honda.01@gmail.com>
2025-12-26 11:17:08 +0900

    update

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -8,3 +8,11 @@ spoon.Caffeine:start()
 
 -- RotateDisplay
 hs.hotkey.bind({"cmd", "option"}, "r", function() hs.execute("ToggleRotateDisplay.sh", true) end)
+
+-- InputSourceSwitch
+hs.loadSpoon("InputSourceSwitch")
+spoon.InputSourceSwitch:setApplications({
+    ["iTerm2"] = "Alphanumeric (Google)",
+})
+
+spoon.InputSourceSwitch:start()
