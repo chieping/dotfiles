2736968f6e42a3bcf8f4b1a0259b6cabfecca29a
chieping <takashi.honda.01@gmail.com>
2026-08-28 20:02:00 +0900

    update

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -17,3 +17,28 @@ spoon.InputSourceSwitch:setApplications({
 })
 
 spoon.InputSourceSwitch:start()
+
+-- Raycastでクリップボード履歴を開く時にカーソル位置のアイテムが選択されることを防ぐ
+local function openClipboardHistorySafely()
+    -- 現在カーソルがあるディスプレイの作業領域を取得
+    local screen = hs.mouse.getCurrentScreen()
+    local frame = screen:frame()
+
+    -- Raycastのポップアップと重ならない場所へ移動
+    local safePoint = {
+        x = frame.x + 20,
+        y = frame.y + 20
+    }
+
+    hs.mouse.absolutePosition(safePoint)
+
+    -- カーソル移動が反映されてからRaycastを開く
+    hs.timer.doAfter(0.05, function()
+        hs.eventtap.keyStroke({"ctrl", "option", "cmd"}, "h", 10000)
+    end)
+end
+
+hs.hotkey.bind(
+    {"ctrl", "cmd"}, "h",
+    openClipboardHistorySafely
+)
